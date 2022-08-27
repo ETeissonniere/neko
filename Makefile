fb9e1504926215ee183d6182da750cb016d5f8a8
Eliott Teissonniere <git.eliott@teissonniere.org>
2022-08-27 10:50:15 +0200

    template

---
+++ Makefile
@@ -0,0 +1,17 @@
+.PHONY: all
+all: fmt lint clean build
+
+lint:
+	deno lint
+
+build:
+	deno compile -o ./neko src/main.ts
+
+clean:
+	rm neko
+
+cache:
+	deno cache src/*.ts
+
+fmt:
+	deno fmt 
---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import yargs from "https://deno.land/x/yargs/deno.ts";
+import { Arguments } from "https://deno.land/x/yargs/deno-types.ts";
+import * as log from "https://deno.land/std/log/mod.ts";
+
+const main = async () => {
+  await log.setup({
+    handlers: {
+      console: new log.handlers.ConsoleHandler("DEBUG"),
+    },
+    loggers: {
+      default: {
+        level: "DEBUG",
+        handlers: ["console"],
+      },
+    },
+  });
+
+  yargs(Deno.args)
+    .command({
+      command: "fetch",
+      describe:
+        "connect to a substrate chain and fetch all the data necessary for neko to run her analysis",
+      // deno-lint-ignore no-explicit-any
+      builder: (y: any) =>
+        y.option("url", {
+          alias: "u",
+          type: "string",
+          description: "node url to connect to",
+          demandOption: true,
+        }),
+      handler: async (_argv: Arguments) => {
+        // todo
+      },
+    });
+};
+
+main().catch(console.error).then(() => Deno.exit(0));
